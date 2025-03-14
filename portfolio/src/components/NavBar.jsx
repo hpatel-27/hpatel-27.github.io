6b68d9212b1f617b3a87059196104b5f86c59ce3
Harsh <harshpxv@gmail.com>
2025-03-14 13:25:32 -0400

    Got rid of weird navbar collapsing button

--- portfolio/src/components/NavBar.jsx
+++ portfolio/src/components/NavBar.jsx
@@ -7,17 +7,6 @@ const NavBar = () => {
         <a className="navbar-brand" href="#home">
           Harsh Patel
         </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
