6ad508669194fc52b83dcf0d48bed4d4cc9649c0
Harsh <harshpxv@gmail.com>
2025-03-14 12:39:48 -0400

    Built new default project, trying to avoid having a gh-pages branch for this and manually running npm run deploy

--- portfolio/src/components/Intro.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Intro = () => {
-  return <div>Introduction to who is Harsh Patel</div>;
-};
-
-export default Intro;
--- portfolio/src/components/NavBar.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const NavBar = () => {
-  return <div>Navigation Bar from yours truly</div>;
-};
-
-export default NavBar;
