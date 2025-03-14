f2d90db0c91fc720b887398e1399c6b2f1df59e7
Harsh <harshpxv@gmail.com>
2025-03-14 12:49:03 -0400

    Reset back to the old React app

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Intro from "./components/Intro";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Intro />
+    </>
+  );
+}
+
+export default App;
---
+++ portfolio/src/components/NavBar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NavBar = () => {
+  return <div>Navigation Bar from yours truly</div>;
+};
+
+export default NavBar;
