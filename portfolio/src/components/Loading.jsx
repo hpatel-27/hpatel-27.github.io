7a54160b9998482cc73bcb4bcc710994c194cc4e
Harsh <harshpxv@gmail.com>
2025-04-18 19:50:16 -0400

    Bug: Loading the first character of text only

---
+++ portfolio/src/components/Loading.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+
+export const Loading = () => {
+  const [text, setText] = useState("");
+  const fullText = "<Hello World />";
+
+  useEffect(() => {
+    let index = 0;
+    const interval = setInterval(() => {
+      setText(fullText.substring(0, index));
+      index++;
+
+      if (index > fullText.length) {
+        clearInterval(interval);
+      }
+    }, 100);
+
+    return () => clearInterval(interval);
+  });
+
+  return (
+    <div className="fixed inset-0 z-0 bg-black text-gray-100 flex flex-col items-center justify-center">
+      <div className="mb-4 text-4xl font-mono font-bold">
+        {text} <span className="animate-blink ml-1"> | </span>
+      </div>
+      <div className="w-[200px] h-[2px] bg-gray-800 rounded relative overflow-hidden">
+        <div className="w-[40%] h-full bg-purple-600 shadow-[0_0_15px_#3b82f6] animate-loading-bar"></div>
+      </div>
+    </div>
+  );
+};
