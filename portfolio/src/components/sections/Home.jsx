343d68e106c6ad9d91310ed28fafd86d2dda85b3
Harsh <harshpxv@gmail.com>
2025-04-19 13:27:49 -0400

    Update: Completed Home section

--- portfolio/src/components/sections/Home.jsx
+++ portfolio/src/components/sections/Home.jsx
@@ -9,7 +9,7 @@ export const Home = () => {
           Hi, I'm Harsh Patel
         </h1>
 
-        <p>
+        <p className="text-gray-400 text-lg mb-8 max-w-lg mx-auto">
           I'm a senior at North Carolina State University pursuing a degree in
           Computer Science with a concentration in AI! I'll be graduating in May
           2025 and am seeking opportunities as a Software Engineer or AI/ML
@@ -18,6 +18,25 @@ export const Home = () => {
           applications. My goal is to develop solutions that offer both
           exceptional performance and unique experiences.
         </p>
+
+        <div className="flex justify-center space-x-4">
+          <a
+            href="#projects"
+            className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative
+                overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]"
+          >
+            View Projects
+          </a>
+
+          <a
+            href="#contact"
+            className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium
+                transition-all duration-200 hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)
+                hover:bg-blue-500/10"
+          >
+            Contact Me
+          </a>
+        </div>
       </div>
     </section>
   );
