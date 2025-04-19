49f70cb708d6c889e1c92191f28ca59fdea9f0b6
Harsh <harshpxv@gmail.com>
2025-04-19 00:36:05 -0400

    Use the Navbar and MobileMenu now that they are complete

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,11 +1,25 @@
 import { useState } from "react";
 import { Loading } from "./components/Loading";
+import { NavBar } from "./components/NavBar";
+import { MobileMenu } from "./components/MobileMenu";
 import "./styles/index.css";
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
-
-  return <>{!isLoaded && <Loading onComplete={() => setIsLoaded(true)} />}</>;
+  const [menuOpen, setMenuOpen] = useState(false);
+  return (
+    <>
+      {!isLoaded && <Loading onComplete={() => setIsLoaded(true)} />}{" "}
+      <div
+        className={`min-h-screen transition-opacity duration-700 ${
+          isLoaded ? "opacity-100" : "opacity-0"
+        } bg-black text-gray-100`}
+      >
+        <NavBar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      </div>
+    </>
+  );
 }
 
 export default App;
