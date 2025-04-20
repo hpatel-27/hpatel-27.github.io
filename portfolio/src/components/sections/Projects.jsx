024f48e39a28a8725c3bd9c71e42c022b841922a
Harsh <harshpxv@gmail.com>
2025-04-20 00:44:31 -0400

    Use 1 grid column for mobile, 2 grid columns for web

--- portfolio/src/components/sections/Projects.jsx
+++ portfolio/src/components/sections/Projects.jsx
@@ -12,7 +12,7 @@ export const Projects = () => {
             Projects
           </h2>
           <div className="flex flex-col gap-6">
-            <div className="grid grid-cols-2 p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+            <div className="grid grid-cols-1 md:grid-cols-2 p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
               <div>
                 <h3 className="text-xl font-bold mb-2">
                   Full-Stack Fitness App
@@ -59,7 +59,7 @@ export const Projects = () => {
               <div>TODO: Insert video or picture here</div>
             </div>
 
-            <div className="grid grid-cols-2 p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+            <div className="grid grid-cols-1 md:grid-cols-2 p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
               <div>
                 <h3 className="text-xl font-bold mb-2">
                   Full-Stack Fitness App
@@ -106,7 +106,7 @@ export const Projects = () => {
               <div>TODO: Insert video or picture here</div>
             </div>
 
-            <div className="grid grid-cols-2 p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+            <div className="grid grid-cols-1 md:grid-cols-2 p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
               <div>
                 <h3 className="text-xl font-bold mb-2">
                   Full-Stack Fitness App
@@ -153,7 +153,7 @@ export const Projects = () => {
               <div>TODO: Insert video or picture here</div>
             </div>
 
-            <div className="grid grid-cols-2 p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+            <div className="grid grid-cols-1 md:grid-cols-2 p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
               <div>
                 <h3 className="text-xl font-bold mb-2">
                   Full-Stack Fitness App
