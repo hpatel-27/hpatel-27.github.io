d525979ef49c271c66a275f25c0658ff3f5ce6c5
Harsh <harshpxv@gmail.com>
2025-04-19 23:13:49 -0400

    Updated hover shadow

--- portfolio/src/components/sections/About.jsx
+++ portfolio/src/components/sections/About.jsx
@@ -31,7 +31,7 @@ export const About = () => {
                   <span
                     key={key}
                     className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
-                                      hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition"
+                                      hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition"
                   >
                     {tech}
                   </span>
@@ -46,7 +46,7 @@ export const About = () => {
                   <span
                     key={key}
                     className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
-                                      hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition"
+                                      hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition"
                   >
                     {tech}
                   </span>
@@ -61,7 +61,7 @@ export const About = () => {
                   <span
                     key={key}
                     className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
-                                      hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition"
+                                      hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition"
                   >
                     {tech}
                   </span>
