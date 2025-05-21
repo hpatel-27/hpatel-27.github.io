5482e7882b8be8d86dff15158b1ed26fca00ffce
Harsh <harshpxv@gmail.com>
2025-05-21 12:29:02 -0400

    feat: changing button colors

--- portfolio/src/components/sections/Home.jsx
+++ portfolio/src/components/sections/Home.jsx
@@ -8,7 +8,7 @@ export const Home = () => {
     >
       <RevealOnScroll>
         <div className="text-center z-10 px-4">
-          <h1 className="items-center text-5xl md:text-7xl font-bold mb-6 mt-20 bg-gradient-to-r from-blue-500 to-purple-500 bg-clip-text text-transparent leading-right">
+          <h1 className="items-center text-5xl md:text-7xl font-bold mb-6 mt-20 bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent leading-right">
             Hi, I'm Harsh Patel
           </h1>
 
@@ -30,7 +30,7 @@ export const Home = () => {
           <div className="flex justify-center space-x-4">
             <a
               href="#projects"
-              className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative
+              className="bg-indigo-600 text-white py-3 px-6 rounded font-medium transition relative
                 overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]"
             >
               View Projects
@@ -38,9 +38,9 @@ export const Home = () => {
 
             <a
               href="#contact"
-              className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium
+              className="border border-indigo-600/50 text-indigo-600 py-3 px-6 rounded font-medium
                 transition-all duration-200 hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)
-                hover:bg-blue-500/10"
+                hover:bg-indigo-600/10"
             >
               Contact Me
             </a>
